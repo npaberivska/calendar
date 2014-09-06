7e126d4cc4253e0af428d7cd530ac1992140b5f4
natalia <natalia.paberivska@gmail.com>
2014-09-06 19:17:26 +0300

    update files

--- myjs.js
+++ myjs.js
@@ -1,41 +1,64 @@
-function Calendar(id, year, month) {
-  var elem = document.getElementById(id);
-  table = document.createElement('table');
-  tableHd = '<tr><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th><th>Sun</th></td>';   
-  date = new Date(year, month);
-  firstDay = date.getDate() == 0 ? 7 : date.getDate();
-
-  daysInMon = new Date(year, month+1, 0).getDate();
-  count = rowC = 0;
-
-  table.innerHTML = tableHd ;
-
-  rowC = Math.ceil((daysInMon + firstDay - 1) / 7);
-  count = rowC * 7;
-
-  var row = document.createElement('tr');
-  j = 1;
-
-  for(var i=1; i <= count; i++){
-  	var td = document.createElement('td');
-  	content;
-
-  	if(i >= firstDay && i < (daysInMon + firstDay)){
-  		content = j++;
-  	} else {
-  		content = '';
-  	}
-
-  	td.innerHTML = content;
-  	row.appendChild(td);
-
-  	if(i % 7 === 0){
-  		table.appendChild(row);
-  		row = document.createElement('tr');
-  	}
-  };
-
-  elem.appendChild(table);
+function montharr(m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12) {
+  this[1] = m1;
+  this[2] = m2;
+  this[3] = m3;
+  this[4] = m4;
+  this[5] = m5;
+  this[6] = m6;
+  this[7] = m7;
+  this[8] = m8;
+  this[9] = m9;
+  this[10] = m10;
+  this[11] = m11;
+  this[12] = m12;
+}
+function calendar() {
+  var monthNames = "   JanFebMarAprMayJunJulAugSepOctNovDec";
+  var today = new Date();
+  var thisDay;
+  var monthDays = new montharr(31, 28, 31, 30, 31, 30, 31, 31, 30,31, 30, 31);
+  year = today.getYear();
+  if (year < 2000)
+    year = year + 1900;
+  thisDay = today.getDate();
+  if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+    monthDays[1] = 29;
+  nDays = monthDays[today.getMonth()];
+  firstDay = today;
+  firstDay.setDate(1); 
+  testMe = firstDay.getDate();
+  if (testMe == 2)
+    firstDay.setDate(0);
+  startDay = firstDay.getDay();
+  document.writeln("<div align = 'center'>");
+  document.write(monthNames.substring(today.getMonth() * 3 + 3, (today.getMonth() + 1) * 3 + 3));
+  document.write(". ");
+  document.write(year);
+  document.write("<table border = '1'>");
+  document.write("<tr><th>Mon <th>Tue <th>Wed <th>Thu <th>Fri <th>Sat <th>Sun" );
+  document.write("<tr>");
+  column = 0;
+  for (i = 1; i < startDay; i++) {
+    document.write("<td width='30'>");
+    column++;
+  }
+  for (i = 1; i <= nDays; i++) {
+    document.write("<td width=30>");
+    if (i == thisDay)
+      document.write("<span style = 'color: green;'>")
+    document.write(i);
+    if (i == thisDay)
+      document.write("</span>")
+    column++;
+    if (column == 7) {
+      document.write("<tr>"); 
+      column = 0;
+    }
+  }
+  document.write("</table>");
+  document.writeln("</div>");
 }
 
-Calendar('calendar', 2014, 1);
+window.onload = function(){
+  calendar();
+}
